f968959ea0c83d18561994caabc64493c3b38e38
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-03-05 07:45:27 +0530

    added gyan section

--- src/app/Card.tsx
+++ src/app/Card.tsx
@@ -1,30 +1,53 @@
 "use client"
 import { useTomatoStore } from "@/store/store";
 import { GiHamburger } from "react-icons/gi"
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai"
 import Image from "next/image"
 import { Data } from "./page"
-type Props = {
-    data: Data[]
-    id: number
-}
+import { useState } from "react";
+import { m as motion } from "framer-motion"
 function Card(props: Data) {
+    const [isFav, setIsFav] = useState(false)
     const addData = useTomatoStore(state => state.addData);
+    const animate = {
+        scale: 1.1,
+        transition: {
+            duration: 0.2,
+        },
+    };
     return (
-        <div className="rounded-3xl bg-[#303030]  w-fit transition-all hover:scale-105 overflow-clip  flex flex-col pb-4">
-            <Image src={`/menuItems${props.image}`} className="mx-auto   rounded-3xl aspect-square object-contain" width={300} height={250} alt={props.name} />
-            <div className="flex flex-col justify-between flex-1">
-                <h1 className=" text-xl mt-2 w-fit mx-auto text-center">{props.name}</h1>
+        <div className="card" onDoubleClick={() => {
+            setIsFav(!isFav)
+        }}>
+            <Image src={`/menuItems${props.image}`} className="mx-auto   aspect-square object-contain" width={300} height={200} alt={props.name} />
+            <div className="flex flex-col justify-between flex-1 relative">
+                <button className="absolute  -translate-y-full left-0 w-full px-5 pb-4 bg-gradient-to-t from-black to-[#00000005] "
+                    onClick={() => {
+                        setIsFav(!isFav)
+                    }}
+                >
+                    {
+                        isFav ? <AiFillHeart className="text-3xl text-red-500 ml-auto" onClick={() => setIsFav(false)} /> : <AiOutlineHeart className="text-3xl text-red-500 ml-auto" onClick={() => setIsFav(true)} />
+                    }
+                </button>
+                <h1 className=" text-xl mt-2 w-fit mx-auto text-center mb-2">{props.name}</h1>
                 <p className="flex justify-between items-center mt-2 px-4">
                     <span className=" text-xl font-sans">${props.price}</span>
-                    <button className="px-4 py-2 text-white rounded-3xl flex items-center gap-4 bg-mSecondary"
+                    <motion.button
+                        whileHover={animate}
+                        whileTap={
+                            {
+                                scale: 0.9,
+                                backgroundColor: "#966220",
+                            }
+                        }
+                        className="px-4 py-1 text-white rounded-xl flex items-center gap-4 bg-mSecondary"
                         onClick={() => {
                             addData(props)
                         }}
-                    ><span>Order</span> <GiHamburger /></button>
+                    ><span>Order</span> <GiHamburger /></motion.button>
                 </p>
             </div>
-
-
         </div>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 "use client"
 import "./globals.css";
 import { domAnimation, LazyMotion } from "framer-motion";
+import { Playfair_Display } from "next/font/google"
+const playfair = Playfair_Display({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-playfair"
+})
 export default function RootLayout({
   children,
 }: {
@@ -8,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="w-screen overflow-x-hidden">
+      <body className={`w-screen overflow-x-hidden ${playfair.variable}`}>
         <LazyMotion features={domAnimation} strict>
           {children}
         </LazyMotion>
